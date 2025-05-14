4acae4208a725589a1c5e30230286e80a9937483
TmTanky <tmtanky@gmail.com>
2025-05-15 04:04:50 +0800

    feat: added upload pdf component
    PDF-0000

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UploadPDF } from '@/components/ui/uploadPDF'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <h1 className="text-4xl font-bold">PDF Quiz Generator</h1>
-    </div>
-  );
+	return (
+		<main className='grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+			<h1 className='text-4xl font-bold'>PDF Quiz Generator</h1>
+			<UploadPDF />
+		</main>
+	)
 }
